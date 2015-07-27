cca1cf48542931eb5b3e8da870f083405bef19f7
王文杰 <wangwenjie@letv.com>
2015-07-27 18:26:36 +0800

    update index

---
+++ statics/js/require.config.js
@@ -0,0 +1,12 @@
+//requirejs config add by wwj  0716
+requirejs.config({
+    baseUrl: 'statics/js',
+    paths: {
+        jquery: [
+             'jquery.min',
+             'http://libs.baidu.com/jquery/1.11.1/jquery.min'
+        ],
+        bootstrap: 'bootstrap.min',
+        template: 'template',
+    }
+});
