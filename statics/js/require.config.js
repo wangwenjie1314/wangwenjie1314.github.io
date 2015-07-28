e66f5d846bf32ed0b5623222da82605852391c7f
wangwenjie <1662484899@qq.com>
2015-07-28 22:54:18 +0800

    update index js

--- statics/js/require.config.js
+++ statics/js/require.config.js
@@ -1,12 +1,20 @@
 //requirejs config add by wwj  0716
 requirejs.config({
     baseUrl: 'statics/js',
+    shim:{
+    	'bootstrap':['jquery'],
+    	'dropdown':['jquery'],
+    	'collapse':['jquery']
+    },
     paths: {
-        jquery: [
+        'jquery': [
              'jquery.min',
              'http://libs.baidu.com/jquery/1.11.1/jquery.min'
         ],
-        bootstrap: 'bootstrap.min',
-        template: 'template',
+        'collapse':'collapse',
+        'dropdown':'dropdown',
+        'domReady':'require.domReady',
+        'bootstrap': 'bootstrap.min',
+        'template': 'template',
     }
 });
